0a96742fd318bf87d03b18bd17c24503c2de45ad
Mediatec <mediatec@Imac-Kista-PBP.local>
2015-02-17 14:24:35 +0100

    some readme, some tests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kipro-transcode
+
+Well this is basically a script that uses my lib "kipro" to check for new material on disk & copy and transcode with ffmpeg.
+
+This can be set to run at night to automatically backup&transcode material.
--- start.js
+++ start.js
@@ -3,18 +3,68 @@
 var request = require('request');
 var _ = require('underscore');
 var fs = require('fs');
-var child_process = require("child_process");
+var mkdirp = require('mkdirp');
+var KiPro = require("kipro").KiPro;
+
+var kipro = new KiPro("10.47.15.135");
+
+var directory = '';
+
+kipro.getClips(function(clips){
+
+	var cliplist = clips;
+
+	_.each(clips.clips, function(clip) {
+
+		var timestamp = new Date(clip.timestamp);
+		var newDate = "" + timestamp.getFullYear()  + numPad0(timestamp.getMonth()+1) + timestamp.getDate();
 
 
+		mkdirp(directory + newDate, function (err) {
+			if (err){
+				console.error(err);
+			} else {
+				fs.exists(directory + newDate + '/' + clip.clipname, function(exists) {
+				   	if(!exists)
+				   	{
+						kipro.getMedia(clip.clipname, directory + newDate + '/' + clip.clipname, function(file, location) {
+							console.log(file, location);
+							//Add to transcode queue
+						});
+				   	}
+				});
+			}
+		});
+
+	});
+
+});
+
+function numPad0( str ){
+	var cStr = str.toString();
+	if( cStr.length < 2 ){
+		 str = 0 + cStr;
+	}
+	return str;
+}
+/*
+kipro.getMedia("RECORDING_1.mov", "test.mov", function(file, location) {
+	console.log(file, location)
+});
+/*
+ kipro.getMedia("RECORDING_1.mov", "test.mov", function(file, location) {
+						console.log(file, location)
+					});
+/*
 request('http://10.47.15.135/clips?action=get_playlists', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Show the HTML for the Google homepage.
   }
 })
 
- 
+ */
 
-var x = request('http://10.47.15.135/media/RECORDING_12.mov');
+//var x = request('http://10.47.15.135/media/RECORDING_12.mov');
 
 /*
 	console.log("ok");
